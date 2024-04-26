26c05535a6ac58245ad59a4862715036cb3532b4
starblade24 <larry.banks.4790@gmail.com>
2024-04-26 18:49:41 -0500

    final

--- elite_102_Pro_python.py
+++ elite_102_Pro_python.py
@@ -6,7 +6,8 @@ def menu():
   print("2. Deposit ")
   print("3. Withdraw ")
   print("4. Delete account")
-  print("5. Update account details")
+  print("5. Make accoount")
+  print("6. Change account details")
   print("--------------------------------------------")
 
 connection = mysql.connector.connect(
@@ -52,22 +53,62 @@ def options():
       for row in cursor:
         print(row)
       connection.commit()
-"""
-  elif userInput == 3:
-    menu()
-  elif userInput == 4:
-    loop = False
-    print("bye bye ;)")
 
-loop = True
+  elif userInput == 3:
+    amount = int(input("How much would you like to withdraw?: "))
+    if amount <= 0:
+      print("Can not put negative amount out")
+    else:
+      testQuery = ("UPDATE bank  SET money = -" + str(amount) + " + money  WHERE acc_name = " + "'" + name + "'") 
+      cursor.execute(testQuery)
+      testQuery = ("SELECT * FROM sample.bank WHERE acc_name = " +  "'" + name + "'" )
+      cursor.execute(testQuery)
+      for row in cursor:
+          print(row)
+      connection.commit()
+
+
+
+  elif userInput == 4:
+    testQuery = ("delete from bank where acc_name = " + "'" + name + "'") 
+    cursor.execute(testQuery)
+    testQuery = ("SELECT * FROM sample.bank WHERE acc_name = " +  "'" + name + "'" )
+    cursor.execute(testQuery)
+    for row in cursor:
+        print(row)
+    connection.commit()
+    print("Account deleted succfully :)")
+
+  elif userInput == 5:
+    stats = int(input("How much money would you like to start with?"))
+    acc_de = input("What one word should be your detail? (no spaces)")
+    if " " in acc_de:
+      print('No spaces')
+    else:
+      testQuery = ("INSERT INTO bank(money, acc_name, details) VALUES (" + str(stats) +  ", '" + name + "' , '" + acc_de +"');") 
+      cursor.execute(testQuery)
+      testQuery = ("SELECT * FROM sample.bank WHERE acc_name = " +  "'" + name + "'" )
+      cursor.execute(testQuery)
+      for row in cursor:
+          print(row)
+      connection.commit()
+
+  elif userInput == 6:
+    new_de = input("what new one word should your details be now?")
+    testQuery = ("UPDATE bank SET details = '" + new_de +   "' WHERE acc_name = '" + name +  "';") 
+    cursor.execute(testQuery)
+    testQuery = ("SELECT * FROM sample.bank WHERE acc_name = " +  "'" + name + "'" )
+    cursor.execute(testQuery)
+    for row in cursor:
+        print(row)
+    connection.commit()
   
 
 
 
-while loop == True:
-    menu()
 
-"""
+
+
 menu()
 options()
 
